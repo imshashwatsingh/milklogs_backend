b8874c83b631401d4beb3cd9eb035ce1f0db1c3f
Shashwat Singh <imshashwatsingh@gmail.com>
2025-07-25 10:42:00 +0530

    auth correction

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authenticate } from "../controllers/authController";
+import { authenticate } from "../controllers/authController.js";
 import { updateProfile, addData, updateData, getEntry, getAllEntry } from "../controllers/dashController.js";
 const router = express.Router();
 
@@ -11,7 +11,6 @@ router.put("/add", authenticate,addData); // done
 router.patch("/update", authenticate,updateData); // done
 
 router.get("/getentry/:date", authenticate,getEntry); // for a specific date
-
 
 router.get("/getallentry/:year", authenticate,getAllEntry); // for all entries 
 
