36d5d0bea6fc5c20f73a8a8beda0c86c06707691
Shashwat Singh <imshashwatsingh@gmail.com>
2025-07-25 11:45:19 +0530

    cors resolve-2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ env.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 
 // Routes
 app.use("/", homeRoutes);
